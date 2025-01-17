df8b45cd5e09c99ef95dee9e22fee6fbd515e463
ultrasage-danz <olokodanielonline@gmail.com>
2025-01-17 04:45:43 +0100

    Initial commit: SmartCall Lite version

---
+++ src/main/java/com/smartcall/SmartCallApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.smartcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCallApplication.class, args);
+    }
+}
